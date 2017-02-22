513fa0080821f3cb2a1ae5d840838269c8e8f3cb
Emily Patterson <emelocoton189@gmail.com>
2017-02-22 08:14:55 -0600

    initial commit

---
+++ par.js
@@ -0,0 +1,11 @@
+// display for particlesjs
+window.onload = function () {
+    Particles.init({
+        selector: '#myCanvas'
+        , color: '#0f9976'
+        , maxParticles: 350
+        , sizeVariations: 3
+        , speed: 1
+        , connectParticles: true
+    });
+};
